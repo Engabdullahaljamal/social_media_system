8ba9df451143bf97bf5cb0e637a7fc8cef93465f
abdullah <en>
2024-05-28 15:19:24 +0300

    new commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
-
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
-
 
 class AuthController extends Controller
 {
